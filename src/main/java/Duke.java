528c400834961e2f127879100e09d72db05e40ee
Waynefong0401 <waynefong0401@gmail.com>
2019-08-25 08:35:25 +0800

    testing

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -21,7 +21,7 @@ public class Duke {
                     return;
                 } else if (cmd.equalsIgnoreCase("list")) {
                     System.out.println("Here are the tasks in your list:");
-                    Iterator<TaskNew> iter = taskList.iterator();
+                    Iterator<Task> iter = taskList.iterator();
                     int curIndex = 1;
                     while (iter.hasNext()) {
                         System.out.println(String.valueOf(curIndex++) + "." + iter.next().toString());
@@ -31,7 +31,7 @@ public class Duke {
                         taskList.addTask(cmd);
                         continue;
                     }
-                    TaskNew doneTask = taskList.doneTask(cmd);
+                    Task doneTask = taskList.doneTask(cmd);
                     System.out.println("Nice! I've marked this task as done: ");
                     System.out.println(doneTask.toString());
                 } else if (cmd.startsWith("delete")){
@@ -39,7 +39,7 @@ public class Duke {
                         taskList.addTask(cmd);
                         continue;
                     }
-                    TaskNew deleteTask = taskList.deleteTask(cmd);
+                    Task deleteTask = taskList.deleteTask(cmd);
                     System.out.println("Noted. I've removed this task: ");
                     System.out.println(deleteTask.toString());
                     System.out.println("Now you have " + taskList.size() + " tasks in the list.");
--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,94 +1,46 @@
 import java.util.ArrayList;
 
-<<<<<<< HEAD
-public class TaskList extends ArrayList<TaskNew> {
-
-=======
 public class TaskList extends ArrayList<Task> {
->>>>>>> parent of 27f6f5d... Level-6
+
     public TaskList() {
         super();
     }
 
-    public Task doneTask(String cmd) throws TaskNotExistException {
-        try {
-            //int inputIndex = Integer.parseInt(cmd.substring(4).trim());
-            return get(Integer.parseInt(cmd.substring(4).trim()) - 1).taskDone();
-        } catch (NumberFormatException e) {
-            throw new NotNumberException();
-        } catch (IndexOutOfBoundsException e) {
-            throw new TaskNotExistException();
-        }
-    }
-
     public void addTask(String cmd) throws NoDescriptionException, NoTimeException {
-        boolean isTask = true;          
-        TaskNew curTask = TaskNew.valueOf(cmd.split(" ",2)[0]);
-        String[] curTask = {"",""};         curTask.setTask(cmd);
-        String[] tempCurTask = cmd.split(" ", 2);           add(curTask);
-        curTask[0] = tempCurTask[0];    
-        if (tempCurTask.length != 1) curTask[1] = tempCurTask[1];   
-        switch (curTask[0]) {   
-        case "todo":    
-            add(new ToDo(curTask[1]));  
-            break;  
-        case "deadline":    
-            String[] ddlDetails = {"",""};  
-            String[] tempDdlDetails = curTask[1].split("/", 2); 
-            ddlDetails[0] = tempDdlDetails[0];  
-            if (tempDdlDetails.length != 1) ddlDetails[1] = tempDdlDetails[1];  
-            add(new Deadline(ddlDetails));  
-            break;  
-        case "event":   
-            String[] eventDetails = {"",""};    
-            String[] tempEventDetails = curTask[1].split("/", 2);   
-            eventDetails[0] = tempEventDetails[0];  
-            if (tempEventDetails.length != 1) eventDetails[1] = tempEventDetails[1];    
-            add(new Event(eventDetails));   
-            break;  
-        default:    
-            isTask = false; 
-            System.out.println("OOPS!!! I'm sorry, but I don't know what that means :-(");  
-        }   
+        boolean isTask = true;
+        String[] curTask = {"",""};
+        String[] tempCurTask = cmd.split(" ", 2);
+        curTask[0] = tempCurTask[0];
+        if (tempCurTask.length != 1) curTask[1] = tempCurTask[1];
+        switch (curTask[0]) {
+        case "todo":
+            add(new ToDo(curTask[1]));
+            break;
+        case "deadline":
+            String[] ddlDetails = {"",""};
+            String[] tempDdlDetails = curTask[1].split("/", 2);
+            ddlDetails[0] = tempDdlDetails[0];
+            if (tempDdlDetails.length != 1) ddlDetails[1] = tempDdlDetails[1];
+            add(new Deadline(ddlDetails));
+            break;
+        case "event":
+            String[] eventDetails = {"",""};
+            String[] tempEventDetails = curTask[1].split("/", 2);
+            eventDetails[0] = tempEventDetails[0];
+            if (tempEventDetails.length != 1) eventDetails[1] = tempEventDetails[1];
+            add(new Event(eventDetails));
+            break;
+        default:
+            isTask = false;
+            System.out.println("OOPS!!! I'm sorry, but I don't know what that means :-(");
+        }
         if (!isTask) return;
         System.out.println("Got it. I've added this task: ");
-        System.out.println(curTask);
-        System.out.println("Now you have " + size() + " tasks in the list.");
-//        boolean isTask = true;
-//        String[] curTask = {"",""};
-//        String[] tempCurTask = cmd.split(" ", 2);
-//        curTask[0] = tempCurTask[0];
-//        if (tempCurTask.length != 1) curTask[1] = tempCurTask[1];
-//        switch (curTask[0]) {
-//        case "todo":
-//            add(new ToDo(curTask[1]));
-//            break;
-//        case "deadline":
-//            String[] ddlDetails = {"",""};
-//            String[] tempDdlDetails = curTask[1].split("/", 2);
-//            ddlDetails[0] = tempDdlDetails[0];
-//            if (tempDdlDetails.length != 1) ddlDetails[1] = tempDdlDetails[1];
-//            add(new Deadline(ddlDetails));
-//            break;
-//        case "event":
-//            String[] eventDetails = {"",""};
-//            String[] tempEventDetails = curTask[1].split("/", 2);
-//            eventDetails[0] = tempEventDetails[0];
-//            if (tempEventDetails.length != 1) eventDetails[1] = tempEventDetails[1];
-//            add(new Event(eventDetails));
-//            break;
-//        default:
-//            isTask = false;
-//            System.out.println("OOPS!!! I'm sorry, but I don't know what that means :-(");
-//        }
-//        if (!isTask) return;
-//        System.out.println("Got it. I've added this task: ");
-//        System.out.println(get(size() - 1).toString());
-//        System.out.println("Now you have " + String.valueOf(size()) + " tasks in the list.");
+        System.out.println(get(size() - 1).toString());
+        System.out.println("Now you have " + String.valueOf(size()) + " tasks in the list.");
     }
-<<<<<<< HEAD
 
-    public TaskNew doneTask(String cmd) throws TaskNotExistException {
+    public Task doneTask(String cmd) throws TaskNotExistException {
         try {
             return get(Integer.parseInt(cmd.substring(4).trim()) - 1).taskDone();
         } catch (NumberFormatException e) {
@@ -98,7 +50,7 @@ public class TaskList extends ArrayList<Task> {
         }
     }
 
-    public TaskNew deleteTask(String cmd) throws TaskNotExistException {
+    public Task deleteTask(String cmd) throws TaskNotExistException {
         try {
             return remove(Integer.parseInt(cmd.substring(6).trim()) - 1);
         } catch (NumberFormatException e) {
@@ -107,62 +59,4 @@ public class TaskList extends ArrayList<Task> {
             throw new TaskNotExistException();
         }
     }
-=======
->>>>>>> parent of 27f6f5d... Level-6
 }
-
-enum TaskNew {
-    todo(false,"[T]"),event(true,"[E]","at"),deadline(true,"[D]","by");
-
-    private String description, details, prep,abbreviation;
-    private boolean hasDetails, isAt, isDone;
-
-    TaskNew(boolean hasDetails,String abbreviation) {
-        this.hasDetails = hasDetails;
-        this.abbreviation = abbreviation;
-        this.isDone = false;
-    }
-
-    TaskNew(boolean hasDetails, String abbreviation,String prep) {
-        this.hasDetails = hasDetails;
-        this.abbreviation = abbreviation;
-        this.prep = prep;
-        this.isDone = false;
-    }
-
-    public TaskNew setTask (String cmd) throws NoDescriptionException, NoTimeException {
-        String[] curTask = cmd.split(" ", 2);
-        if (curTask.length == 1)
-            throw new NoDescriptionException(name());
-        if (curTask[1].isBlank())
-            throw new NoDescriptionException(name());
-        if (hasDetails) {
-            String[] eventDetails = curTask[1].split("/", 2);
-            if (eventDetails.length == 1)
-                throw new NoTimeException(name());
-            if (eventDetails[1].isBlank())
-                throw new NoTimeException(name());
-            this.description = eventDetails[0];
-            this.details = eventDetails[1];
-        } else {
-            this.description = curTask[1];
-        }
-        return this;
-    }
-
-    public TaskNew taskDone() {
-        this.isDone = true;
-        return this;
-    }
-
-    @Override
-    public String toString() {
-        return abbreviation + (isDone ? "\u2713 " : "\u2718 ") + description + (hasDetails ? " (" + prep + " " + details + ")" : "");
-//        if (hasDetails) {
-//            return abbreviation + description + " (" + prep + " " + details + ")";
-//        } else {
-//            return abbreviation + description;
-//        }
-    }
-}
-

759c1f06a87f1b92d9ce2fece64e79070fe757a8
Waynefong0401 <waynefong0401@gmail.com>
2019-08-25 08:14:07 +0800

    fucked up

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,15 +1,56 @@
 import java.util.ArrayList;
 
+<<<<<<< HEAD
 public class TaskList extends ArrayList<TaskNew> {
 
+=======
+public class TaskList extends ArrayList<Task> {
+>>>>>>> parent of 27f6f5d... Level-6
     public TaskList() {
         super();
     }
 
+    public Task doneTask(String cmd) throws TaskNotExistException {
+        try {
+            //int inputIndex = Integer.parseInt(cmd.substring(4).trim());
+            return get(Integer.parseInt(cmd.substring(4).trim()) - 1).taskDone();
+        } catch (NumberFormatException e) {
+            throw new NotNumberException();
+        } catch (IndexOutOfBoundsException e) {
+            throw new TaskNotExistException();
+        }
+    }
+
     public void addTask(String cmd) throws NoDescriptionException, NoTimeException {
+        boolean isTask = true;          
         TaskNew curTask = TaskNew.valueOf(cmd.split(" ",2)[0]);
-        curTask.setTask(cmd);
-        add(curTask);
+        String[] curTask = {"",""};         curTask.setTask(cmd);
+        String[] tempCurTask = cmd.split(" ", 2);           add(curTask);
+        curTask[0] = tempCurTask[0];    
+        if (tempCurTask.length != 1) curTask[1] = tempCurTask[1];   
+        switch (curTask[0]) {   
+        case "todo":    
+            add(new ToDo(curTask[1]));  
+            break;  
+        case "deadline":    
+            String[] ddlDetails = {"",""};  
+            String[] tempDdlDetails = curTask[1].split("/", 2); 
+            ddlDetails[0] = tempDdlDetails[0];  
+            if (tempDdlDetails.length != 1) ddlDetails[1] = tempDdlDetails[1];  
+            add(new Deadline(ddlDetails));  
+            break;  
+        case "event":   
+            String[] eventDetails = {"",""};    
+            String[] tempEventDetails = curTask[1].split("/", 2);   
+            eventDetails[0] = tempEventDetails[0];  
+            if (tempEventDetails.length != 1) eventDetails[1] = tempEventDetails[1];    
+            add(new Event(eventDetails));   
+            break;  
+        default:    
+            isTask = false; 
+            System.out.println("OOPS!!! I'm sorry, but I don't know what that means :-(");  
+        }   
+        if (!isTask) return;
         System.out.println("Got it. I've added this task: ");
         System.out.println(curTask);
         System.out.println("Now you have " + size() + " tasks in the list.");
@@ -45,6 +86,7 @@ public class TaskList extends ArrayList<TaskNew> {
 //        System.out.println(get(size() - 1).toString());
 //        System.out.println("Now you have " + String.valueOf(size()) + " tasks in the list.");
     }
+<<<<<<< HEAD
 
     public TaskNew doneTask(String cmd) throws TaskNotExistException {
         try {
@@ -65,6 +107,8 @@ public class TaskList extends ArrayList<TaskNew> {
             throw new TaskNotExistException();
         }
     }
+=======
+>>>>>>> parent of 27f6f5d... Level-6
 }
 
 enum TaskNew {
